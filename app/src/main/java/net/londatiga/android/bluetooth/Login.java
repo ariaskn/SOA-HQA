f11fe9c845cc0ad4f13a1f4ea307103e0a92b1de
Kevin Arias <kevinarias_20@hotmail.com>
2023-07-02 18:52:08 -0300

    adding login

---
+++ app/src/main/java/net/londatiga/android/bluetooth/Login.java
@@ -0,0 +1,38 @@
+package net.londatiga.android.bluetooth;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (username.getText().toString().equals("hqa_soa")
+                                && password.getText().toString().equals("hqa_soa") ) {
+                            Toast.makeText(Login.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                            Intent passToInicio = new Intent(Login.this, Inicio.class);
+                            startActivity(passToInicio);
+                        }else {
+                            Toast.makeText(Login.this, "LOGIN FAILED!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+}
--- app/src/main/java/net/londatiga/android/bluetooth/MainActivity.java
+++ app/src/main/java/net/londatiga/android/bluetooth/MainActivity.java
@@ -57,7 +57,8 @@ public class MainActivity extends Activity
 			Manifest.permission.ACCESS_COARSE_LOCATION,
 			Manifest.permission.WRITE_EXTERNAL_STORAGE,
 			Manifest.permission.READ_PHONE_STATE,
-			Manifest.permission.READ_EXTERNAL_STORAGE};
+			Manifest.permission.READ_EXTERNAL_STORAGE,
+			};
 
 
 	@Override
